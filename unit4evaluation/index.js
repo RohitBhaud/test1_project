f10b0d011150df40528c0b3f464107b4cbf4ea29
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-03-07 12:04:14 +0530

    rohit

--- unit4evaluation/index.js
+++ unit4evaluation/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
 
 const app = express()
+
+const books =(req ,res ,next)=>{
+    console.log("Fetching all books");
+    next()
+}
+app.get("/books",books ,(req,res)=>{
+    return res.send("books")
+})
+
+const PORT = 3333;
+app.listen(PORT , ()=>{console.log(running)})
