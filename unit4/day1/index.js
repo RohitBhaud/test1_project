7be0e5152abff9d4e8c46399ec25339052f22752
Rohit Bhaud <92250506+RohitBhaud@users.noreply.github.com>
2022-03-07 11:42:47 +0530

    first commit

---
+++ unit4/day1/index.js
@@ -0,0 +1,14 @@
+const add = require("./add")
+const div = require("./div")
+const mul= require("./mul")
+const sub= require("./sub")
+
+
+const a = 20 
+const b = 40 
+
+console.log(add.addition(a,b))
+console.log(div.subtraction(a,b))
+console.log(mul.multiplication(a,b))
+console.log(sub.subtraction(a,b))
+
